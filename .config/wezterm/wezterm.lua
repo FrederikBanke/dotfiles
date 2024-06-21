6d052d95b69f009b465816b094541342daa5076a
Frederik Banke <frederiksbanke@gmail.com>
2024-06-21 09:48:04 +0200

    add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+local act = wezterm.action
+
+local config = wezterm.config_builder()
+
+-- Apply configurations
+config.color_scheme = 'Hardcore'
+
+config.font = wezterm.font 'Hack Nerd Font'
+config.font_size = 11
+
+config.window_background_opacity = 0.7
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Return configuration to wezterm
+return config
