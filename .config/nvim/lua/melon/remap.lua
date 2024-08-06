221d9afed1a167671e35c9698bbe07ad5d37955d
Frederik Banke <frederiksbanke@gmail.com>
2024-08-06 10:32:07 +0200

    chore: add comment & format file

--- .config/nvim/lua/melon/remap.lua
+++ .config/nvim/lua/melon/remap.lua
@@ -1,4 +1,5 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc = "View directory files (:Ex)" })
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "View directory files (:Ex)" })
 
+-- Move lines in visual mode.
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
