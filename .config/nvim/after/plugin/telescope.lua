f186c69c516e6645d1310d07c31d8693fc942c80
Frederik Banke <frederiksbanke@gmail.com>
2024-08-08 11:03:50 +0200

    feat: list references of a variable or function

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,11 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {desc = "Search files in project" })
+vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Search files in project" })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Search files in git project" })
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end,{ desc = "Search content of project files" })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
+end, { desc = "Search content of project files" })
+
+-- Find references (usage) of a function. Uses LSP.
+vim.keymap.set("n", "<leader>ref", function()
+    builtin.lsp_references()
+end, { desc = "List references of a function or variable" })
