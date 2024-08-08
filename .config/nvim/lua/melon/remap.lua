163b425ba0dd179b31e5a55d068f27104a2dab54
Frederik Banke <frederiksbanke@gmail.com>
2024-08-08 11:05:44 +0200

    fix: add some keymaps that make nordic keyboards easier to use

--- .config/nvim/lua/melon/remap.lua
+++ .config/nvim/lua/melon/remap.lua
@@ -3,3 +3,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "View directory files (:E
 -- Move lines in visual mode.
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Remap - to / for easier access. This mimics the US keyboard layout (I use nordic).
+vim.keymap.set({ "n", "v" }, "-", "/")
+-- Map shift+æ to ; for easier access to commands.
+vim.cmd("set langmap=Æ:")
