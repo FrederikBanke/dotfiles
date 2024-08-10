913d47ede4b5f6570de5f86a68e810df7bd3fc3c
Frederik Banke <frederiksbanke@gmail.com>
2024-08-10 21:20:25 +0200

    chore: add helper function to merge tables

--- .config/nvim/after/plugin/lsp-zero.lua
+++ .config/nvim/after/plugin/lsp-zero.lua
@@ -1,12 +1,13 @@
 local lsp_zero = require('lsp-zero')
 local cmp = require("cmp")
+local h = require("helpers")
 
 lsp_zero.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
     lsp_zero.default_keymaps({ buffer = bufnr })
     local opts = { buffer = bufnr, remap = false }
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, h.merge(opts, { desc = "Go to definition" }))
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
@@ -14,13 +15,16 @@ lsp_zero.on_attach(function(client, bufnr)
         function() vim.diagnostic.goto_next({ severity = { min = vim.diagnostic.severity.INFO } }) end, opts)
     vim.keymap.set("n", "<C-,>",
         function() vim.diagnostic.goto_prev({ severity = { min = vim.diagnostic.severity.INFO } }) end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, h.merge(opts, {
+        desc =
+        "Show code actions"
+    }))
+    --vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, h.merge(opts, { desc = "Rename variable" }))
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     vim.keymap.set({ 'n', 'x' }, 'gq', function()
         vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
-    end, { buffer = bufnr, desc = "Format file" })
+    end, h.merge(opts, { desc = "Format file" }))
 end)
 
 -- Set global variable 'vim' for lua files.
