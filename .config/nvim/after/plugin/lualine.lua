7867554590375b975b6d7db378e1c97910b4a88b
Frederik Banke <frederiksbanke@gmail.com>
2024-08-13 13:43:45 +0200

    fix: show recording macro in lualine

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,2 +1,15 @@
 local lualine = require("lualine")
-lualine.setup()
+lualine.setup({
+    sections = {
+        lualine_x = {
+            {
+                require("noice").api.statusline.mode.get,
+                cond = require("noice").api.statusline.mode.has,
+                color = { fg = "#ff9e64" },
+            },
+            'encoding',
+            'fileformat',
+            'filetype',
+        },
+    },
+})
