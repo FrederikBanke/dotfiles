8f7ab525e703651bbbf9cf1e0e1a6fefb26f51cb
Frederik Banke <frederiksbanke@gmail.com>
2024-10-04 14:54:51 +0200

    feat: add waybar & pywal

---
+++ .config/wal/updatewal.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Script for updating wallpaper and colors matching the new wallpaper.
+# Uses pywal.
+
+wal -q -i "$HOME/Sync/Desktop Wallpapers/"
+
+# Add wallpaper to variable
+wallpaper=$(< $HOME/.cache/wal/wal)
+echo "Wallpaper set to: $wallpaper"
+# Load current pywal color scheme
+source "$HOME/.cache/wal/colors.sh"
+
+# Copy color file to waybar folder
+cp ~/.cache/wal/colors-waybar.css ~/dotfiles/.config/waybar/
+
+# Get wallpaper image name
+# wallpaper_name=$(echo $wallpaper | sed "s|$HOME/Sync/Desktop Wallpapers/||g")
+
+# Set the new wallpaper
+hyprctl hyprpaper preload "$wallpaper" &
+hyprctl hyprpaper wallpaper ",$wallpaper" &
+
+# Reload waybar
+~/dotfiles/.config/waybar/launch.sh
---
+++ .config/waybar/launch.sh
@@ -0,0 +1,6 @@
+# Launch script for waybar.
+# Kill all running waybar instances
+killall waybar
+
+# Load waybar with config
+waybar -c ~/.config/waybar/config.jsonc & -s ~/.config/waybar/style.css
