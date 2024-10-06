ca264525bf477e6447be920cbaa32ed5fb42e599
Frederik Banke <frederiksbanke@gmail.com>
2024-10-06 20:14:50 +0200

    fix(waybar): add & to end of command

--- .config/waybar/launch.sh
+++ .config/waybar/launch.sh
@@ -1,6 +1,7 @@
 # Launch script for waybar.
 # Kill all running waybar instances
 killall waybar
+pkill waybar
 
 # Load waybar with config
-waybar -c ~/.config/waybar/config.jsonc & -s ~/.config/waybar/style.css
+waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css &
