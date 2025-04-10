1b21639165aee6779432fabacdcfd88568c18ca9
Frederik Banke <frederiksbanke@gmail.com>
2025-04-10 13:57:46 +0200

    fix(nvim): mappings to move tabs

--- .config/nvim/lua/melon/remap.lua
+++ .config/nvim/lua/melon/remap.lua
@@ -1,5 +1,6 @@
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "View directory files (:Ex)" })
-vim.keymap.set("n", "<leader>pv", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", { desc = "View directory files (:Ex)" })
+vim.keymap.set("n", "<leader>pv", ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+    { desc = "View directory files (:Ex)" })
 
 -- Move lines in visual mode.
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -13,3 +14,6 @@ vim.cmd("set langmap=Æ:")
 -- Map H and L to switch tabs back and forth.
 vim.keymap.set("n", "H", "gT")
 vim.keymap.set("n", "L", "gt")
+-- Move tabs left and right
+vim.keymap.set("n", "<CS-H>", ":tabm -1<CR>")
+vim.keymap.set("n", "<CS-L>", ":tabm +1<CR>")
