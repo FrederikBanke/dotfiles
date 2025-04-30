e0625c6612cb3bee54c16e45de899ecb4cbb9ee9
Frederik Banke <frederiksbanke@gmail.com>
2025-04-30 13:48:04 +0200

    fix(nvim): truncate git branch & hide duplicate vim mode

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,11 +1,41 @@
 local lualine = require("lualine")
 local noice = require("noice")
+
+--- @param trunc_width number trunctates component when screen width is less then trunc_width
+--- @param trunc_len number truncates component to trunc_len number of chars
+--- @param hide_width number|nil hides component when window width is smaller then hide_width
+--- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
+--- @param max_len number the max length a text can be, regardless of window size
+--- return function that can format the component accordingly
+local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis, max_len)
+    return function(str)
+        local win_width = vim.fn.winwidth(0)
+        if hide_width and win_width < hide_width then
+            return ''
+        elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
+            return str:sub(1, trunc_len) .. (no_ellipsis and '' or '...')
+        elseif #str > max_len then
+            return str:sub(1, max_len) .. (no_ellipsis and '' or '...')
+        end
+        return str
+    end
+end
+
 lualine.setup({
     sections = {
+        lualine_b = {
+            { 'branch', fmt = trunc(100, 10, nil, false, 50) },
+            'diff',
+            'diagnostics',
+        },
         lualine_x = {
             {
-                noice.api.statusline.mode.get,
-                cond = noice.api.statusline.mode.has,
+                noice.api.status.mode.get,
+                -- cond = noice.api.statusline.mode.has,
+                cond = function()
+                    local reg = vim.fn.reg_recording()
+                    return reg ~= ""
+                end,
                 color = { fg = "#ff9e64" },
             },
             'encoding',
