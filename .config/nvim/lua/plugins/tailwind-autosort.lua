0f28cf2bea4118716e382f3a63414958d3504d27
Frederik Banke <frederiksbanke@gmail.com>
2025-06-18 14:38:52 +0200

    fix(nvim): disable tailwind-autosort plugin

--- .config/nvim/lua/plugins/tailwind-autosort.lua
+++ .config/nvim/lua/plugins/tailwind-autosort.lua
@@ -1,8 +1,9 @@
 return {
-    "y3owk1n/tailwind-autosort.nvim",
-    version = "*", -- remove this if you want to follow `main` branch
-    event = "VeryLazy",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    ---@type TailwindAutoSort.Config
-    opts = {} -- your configuration
+	"y3owk1n/tailwind-autosort.nvim",
+	enabled = false,
+	version = "*", -- remove this if you want to follow `main` branch
+	event = "VeryLazy",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	---@type TailwindAutoSort.Config
+	opts = {}, -- your configuration
 }
