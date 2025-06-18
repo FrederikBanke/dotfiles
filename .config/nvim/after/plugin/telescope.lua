02e78194efb2fb13c4ea2e2c5caf1bf7f31c2bcb
Frederik Banke <frederiksbanke@gmail.com>
2025-06-18 14:18:24 +0200

    fix(nvim): make search a live grep

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,11 +1,11 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Search files in project" })
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Search files in git project" })
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Search files in project" })
+vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Search files in git project" })
+vim.keymap.set("n", "<leader>ps", function()
+	builtin.live_grep()
 end, { desc = "Search content of project files" })
 
 -- Find references (usage) of a function. Uses LSP.
 vim.keymap.set("n", "<leader>ref", function()
-    builtin.lsp_references()
+	builtin.lsp_references()
 end, { desc = "List references of a function or variable" })
