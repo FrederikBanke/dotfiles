4abc43c66d6ce89a2ed6c34c4c8ab694cf5c6641
Frederik Banke <frederiksbanke@gmail.com>
2025-06-18 15:19:17 +0200

    fix(nvim): finally supports different cases of tailwind attributes and functions

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -114,10 +114,24 @@ vim.lsp.config("lua_ls", {
 
 vim.lsp.config("tailwindcss", {
 	settings = {
+		capabilities = require("cmp_nvim_lsp").default_capabilities(),
 		tailwindCSS = {
+			classAttributes = {
+				"class",
+				"className",
+				"classNames",
+				"class:list",
+				"classList",
+				"ngClass",
+				"twMerge",
+			},
 			experimental = {
 				classRegex = {
-					{ ".+: ([\"'`][^\"'`]*.*?[\"'`])", ".+: [\"'`]([^\"'`]*).*?[\"'`]" },
+					-- Gotten from this list https://github.com/paolotiu/tailwind-intellisense-regex-list
+					-- { "([a-zA-Z0-9\\-:]+)" }, -- everywhere
+					-- { "@tw\\s\\*/\\s+[\"'`]([^\"'`]*)" }, -- comment tag with @tw
+					{ "tv\\(([^)]*)\\)", "{?\\s?[\\w].*:\\s*?[\"'`]([^\"'`]*).*?,?\\s?}?" }, -- tailwind-variants
+					{ "(?:twMerge|twJoin)\\(([^;]*)[\\);]", "[`'\"`]([^'\"`;]*)[`'\"`]" }, -- tailwind-merge
 				},
 			},
 		},
