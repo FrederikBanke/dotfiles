ba5cfd48ead8e5f5ed0b4204e4e571f647b14ca5
Frederik Banke <frederik@banke.dev>
2025-06-22 13:32:02 +0200

    fix(ags): add biome to ags

--- .config/ags/widget/Bar.tsx
+++ .config/ags/widget/Bar.tsx
@@ -1,31 +1,35 @@
-import { App, Astal, Gtk, Gdk } from "astal/gtk3"
-import { Variable } from "astal"
-import AppTitle from "./AppTitle"
+import { Variable } from "astal";
+import { App, Astal, type Gdk, Gtk } from "astal/gtk3";
+import AppTitle from "./AppTitle";
 
-const time = Variable("").poll(1000, "date")
+const time = Variable("").poll(1000, "date");
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
-    return <window
-        className="Bar"
-        gdkmonitor={gdkmonitor}
-        exclusivity={Astal.Exclusivity.EXCLUSIVE}
-        anchor={Astal.WindowAnchor.TOP
-            | Astal.WindowAnchor.LEFT
-            | Astal.WindowAnchor.RIGHT}
-        application={App}>
-        <centerbox>
-            <AppTitle />
-            <button
-                onClicked="echo hellov"
-                halign={Gtk.Align.CENTER} >
-                Welcome to AGS?
-            </button>
-            <box />
-            <button
-                onClick={() => print("hello")}
-                halign={Gtk.Align.CENTER} >
-                <label label={time()} />
-            </button>
-        </centerbox>
-    </window>
+    return (
+        <window
+            className="Bar"
+            gdkmonitor={gdkmonitor}
+            exclusivity={Astal.Exclusivity.EXCLUSIVE}
+            anchor={
+                Astal.WindowAnchor.TOP |
+                Astal.WindowAnchor.LEFT |
+                Astal.WindowAnchor.RIGHT
+            }
+            application={App}
+        >
+            <centerbox>
+                <AppTitle />
+                <button onClicked="echo helloaa" halign={Gtk.Align.CENTER}>
+                    Welcome to AGS?
+                </button>
+                <box />
+                <button
+                    onClick={() => print("hello")}
+                    halign={Gtk.Align.CENTER}
+                >
+                    <label label={time()} />
+                </button>
+            </centerbox>
+        </window>
+    );
 }
