4a5211df653c02e01db91ef8bcfbd37f70327398
Frederik Banke <frederik@banke.dev>
2025-06-22 13:32:02 +0200

    fix(ags): add biome to ags

--- .config/ags/widget/AppTitle.tsx
+++ .config/ags/widget/AppTitle.tsx
@@ -1,17 +1,20 @@
-import AstalHyprland from "gi://AstalHyprland"
-import { bind } from "../../../../../../usr/share/astal/gjs"
+import AstalHyprland from "gi://AstalHyprland";
+import { bind } from "../../../../../../usr/share/astal/gjs";
 
 export default function AppTitle() {
     const hypr = AstalHyprland.get_default();
     const focused = bind(hypr, "focusedClient");
 
-    return <box
-        className="Focused"
-        visible={focused.as(Boolean)}
-    >
-    {/**        {focused.as(client => (
-            client && <label label={bind(client, "title").get()} />
-        ))}*/}
-        <label label={"Some label"} />
-    </box>
+    console.log(
+        "Client:",
+        focused.as((client) => bind(client, "class").get()),
+    );
+    return (
+        <box className="Focused" visible={focused.as(Boolean)}>
+            {focused.as(
+                (client) =>
+                    client && <label label={bind(client, "class").get()} />,
+            )}
+        </box>
+    );
 }
