44eec6f9fe7ba68b72179868bf4d86f6f91256d9
Frederik Banke <frederiksbanke@gmail.com>
2025-12-26 13:19:03 +0100

    fix(yazi): add git plugin

---
+++ .config/yazi/init.lua
@@ -0,0 +1,11 @@
+th.git = th.git or {}
+th.git.modified = ui.Style():fg("blue")
+th.git.deleted = ui.Style():fg("red"):bold()
+th.git.modified_sign = "M"
+th.git.added_sign = "A"
+th.git.untracked_sign = "?"
+th.git.ignored_sign = "I"
+th.git.deleted_sign = "D"
+th.git.updated_sign = "U"
+
+require("git"):setup()
