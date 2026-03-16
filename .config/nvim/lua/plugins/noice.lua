6a86b6f4ffc119bd2f9a8b2059010877795664d7
Frederik Banke <frederiksbanke@gmail.com>
2026-03-16 14:43:49 +0100

    fix(nvim): disable nvim-notify

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -43,11 +43,11 @@ return {
 		-- OPTIONAL:
 		--   `nvim-notify` is only needed, if you want to use the notification view.
 		--   If not available, we use `mini` as the fallback
-		{
-			"rcarriga/nvim-notify",
-			opts = {
-				background_colour = "#000000",
-			},
-		},
+		-- {
+		-- 	"rcarriga/nvim-notify",
+		-- 	opts = {
+		-- 		background_colour = "#000000",
+		-- 	},
+		-- },
 	},
 }
